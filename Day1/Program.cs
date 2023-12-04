62a6ed281c263147f71356116abca6b76ff3bf9e
Andreas Kristiansen <andreas.kristiansen@pector.se>
2023-12-04 10:04:43 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,120 @@
+﻿using AoCToolbox;
+
+Console.WriteLine("*** AdventOfCode 2023 ***");
+Console.WriteLine("-------------------------");
+Console.WriteLine("* December 1st: *");
+Console.WriteLine("   Part #1");
+Console.WriteLine($"     Calibration value: {RunPart1()}");
+Console.WriteLine("   Part #2");
+Console.WriteLine($"     Real calibration value; {RunPart2()}");
+Console.WriteLine("-------------------------");
+
+
+static int RunPart1()
+{
+    var calibrationValuesNumbers = new List<int>();
+
+    foreach (var value in new StringInputProvider("Input.txt").ToList())
+    {
+        var digitsFromString = string.Concat(value.Where(char.IsDigit));
+        var first = firstDigit(int.Parse(digitsFromString));
+        var last = lastDigit(int.Parse(digitsFromString));
+        var calibrationValue = first + last;
+        calibrationValuesNumbers.Add(int.Parse(calibrationValue));
+    }
+
+    return calibrationValuesNumbers.Sum();
+}
+
+static int RunPart2()
+{
+    var stringDigits = new Dictionary<string, string>() { { "one", "1" }, { "two", "2" }, { "three", "3" }, { "four", "4" }, { "five", "5" }, { "six", "6" }, { "seven", "7" }, { "eight", "8" }, { "nine", "9" }, };
+
+    var calibrationValuesNumbers = new List<int>();
+
+    foreach (var value in new StringInputProvider("Input.txt").ToList())
+    {
+        var first = firstRealDigit(value.Trim(), stringDigits);
+        var last = lastRealDigit(value.Trim(), stringDigits);
+
+        var calibrationValue = first + last;
+        calibrationValuesNumbers.Add(int.Parse(calibrationValue));
+    }
+
+    return calibrationValuesNumbers.Sum();
+}
+
+
+static string firstRealDigit(string input, Dictionary<string, string> stringDigits)
+{
+    var indexOfFirstDigit = input.IndexOfAny("0123456789".ToCharArray());
+
+    var stringDigitIndexList = new List<StringDigitIndex>();
+
+    foreach (var value in stringDigits)
+    {
+        stringDigitIndexList.Add(new StringDigitIndex { Index = input.IndexOf(value.Key), Digit = value.Value });
+    }
+    int indexOfFirstStringDigit = 99;
+
+    if (stringDigitIndexList.Any(x => x.Index != -1))
+    {
+        indexOfFirstStringDigit = stringDigitIndexList.Where(x => x.Index != -1).Min(x => x.Index);
+    }
+
+    if (indexOfFirstDigit > -1 && indexOfFirstDigit < indexOfFirstStringDigit)
+    {
+        return firstDigit(int.Parse(input.Substring(indexOfFirstDigit, 1)));
+    }
+    else
+    {
+        return stringDigitIndexList.First(x => x.Index == indexOfFirstStringDigit).Digit;
+    }
+}
+
+
+
+static string lastRealDigit(string input, Dictionary<string, string> stringDigits)
+{
+    var indexOfLastDigit = input.LastIndexOfAny("0123456789".ToCharArray());
+
+    var stringDigitIndexList = new List<StringDigitIndex>();
+
+    foreach (var value in stringDigits)
+    {
+        stringDigitIndexList.Add(new StringDigitIndex { Index = input.LastIndexOf(value.Key), Digit = value.Value });
+    }
+    int indexOfLastStringDigit = -1;
+    if (stringDigitIndexList.Any(x => x.Index != -1))
+    {
+        indexOfLastStringDigit = stringDigitIndexList.Where(x => x.Index != -1).Max(x => x.Index);
+    }
+
+    if (indexOfLastDigit > -1 && indexOfLastDigit > indexOfLastStringDigit)
+    {
+        return firstDigit(int.Parse(input.Substring(indexOfLastDigit, 1)));
+    }
+    else
+    {
+        return stringDigitIndexList.Last(x => x.Index == indexOfLastStringDigit).Digit;
+    }
+}
+static string firstDigit(int n)
+{
+
+    while (n >= 10)
+        n /= 10;
+
+    return n.ToString();
+}
+
+static string lastDigit(int n)
+{
+    return (n % 10).ToString();
+}
+
+class StringDigitIndex
+{
+    public int Index { get; set; }
+    public string Digit { get; set; }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,146 @@
+﻿using AoCToolbox;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("*** AdventOfCode 2023 ***");
+Console.WriteLine("-------------------------");
+Console.WriteLine("* December 2nd: *");
+Console.WriteLine("   Part #1");
+Console.WriteLine($"     Sum Of GameIds: {RunPart1()}");
+Console.WriteLine("   Part #2");
+Console.WriteLine($"     Sum of power of games: {RunPart2()}");
+Console.WriteLine("-------------------------");
+
+
+static int RunPart1()
+{
+    int fixedNumberOfRed = 12;
+    int fixedNumberOfGreen = 13;
+    int fixedNumberOfBlue = 14;
+
+    var gameRecords = parseGameRecords();
+
+    foreach (var gameRecord in gameRecords)
+    {
+        gameRecord.IsPossibleToPlay = isPossibleToPlay(fixedNumberOfRed, fixedNumberOfGreen, fixedNumberOfBlue, gameRecord.GameSessions);
+    }
+
+    return gameRecords.Where(x => x.IsPossibleToPlay).Sum(x => x.GameId);
+}
+
+static int RunPart2()
+{
+    var gameRecords = parseGameRecords();
+
+    int sumOfPowerOfSets = 0;
+
+    foreach (var gameRecord in gameRecords)
+    {
+        int minNumOfRed = gameRecord.GameSessions.Max(x => x.NumberOfRed);
+        int minNumOfBlue = gameRecord.GameSessions.Max(x => x.NumberOfBlue);
+        int minNumOfGreen = gameRecord.GameSessions.Max(x => x.NumberOfGreen);
+
+        int power = minNumOfRed * minNumOfBlue * minNumOfGreen;
+
+        sumOfPowerOfSets += power;
+    }
+
+    return sumOfPowerOfSets;
+}
+
+static bool isPossibleToPlay(int numberOfRed, int numberOfGreen, int numberOfBlue, List<GameSession> gameSessions)
+{
+    foreach (var gameSession in gameSessions)
+    {
+        if (gameSession.NumberOfBlue > numberOfBlue)
+        {
+            return false;
+        }
+
+        if (gameSession.NumberOfGreen > numberOfGreen)
+        {
+            return false;
+        }
+
+        if (gameSession.NumberOfRed > numberOfRed)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static List<GameRecord> parseGameRecords()
+{
+    var gameRecords = new List<GameRecord>();
+    var rawRecords = new StringInputProvider("Input.txt").ToList();
+
+    foreach (var record in rawRecords)
+    {
+        var rawRecord = record.Split(':');
+        var gameRecord = new GameRecord();
+        gameRecord.GameId = getGameId(rawRecord[0]);
+        gameRecord.GameSessions = parseGameSessions(rawRecord[1].Trim());
+
+        gameRecords.Add(gameRecord);
+    }
+
+    return gameRecords;
+}
+static List<GameSession> parseGameSessions(string rawSessionData)
+{
+    var gameSessionList = new List<GameSession>();
+    var gameSessions = rawSessionData.Split(';');
+
+    foreach (var session in gameSessions)
+    {
+        var gameSession = new GameSession();
+        string pattern = @"^(\d.*) \w.*$";
+
+        var colorsOfSession = session.Split(",");
+
+        foreach (var colorOfSession in colorsOfSession)
+        {
+            if (colorOfSession.Contains("red"))
+            {
+                var match = Regex.Match(colorOfSession.Trim(), pattern);
+                gameSession.NumberOfRed = int.Parse(match.Groups[1].Value);
+            }
+
+            if (colorOfSession.Contains("blue"))
+            {
+                var match = Regex.Match(colorOfSession.Trim(), pattern);
+                gameSession.NumberOfBlue = int.Parse(match.Groups[1].Value);
+            }
+
+            if (colorOfSession.Contains("green"))
+            {
+                var match = Regex.Match(colorOfSession.Trim(), pattern);
+                gameSession.NumberOfGreen = int.Parse(match.Groups[1].Value);
+            }
+        }
+
+        gameSessionList.Add(gameSession);
+    }
+
+    return gameSessionList;
+}
+
+static int getGameId(string value)
+{
+    return int.Parse(value.Replace("Game ", ""));
+}
+
+class GameRecord
+{
+    public int GameId { get; set; }
+    public List<GameSession> GameSessions { get; set; }
+    public bool IsPossibleToPlay { get; set; }
+}
+
+class GameSession
+{
+    public int NumberOfBlue { get; set; }
+    public int NumberOfRed { get; set; }
+    public int NumberOfGreen { get; set; }
+}
