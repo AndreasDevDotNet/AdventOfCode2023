3512aafba64a52628193b2739c60db0932f8c8bd
Andreas Kristiansen <andreas.kristiansen@pector.se>
2023-12-08 07:57:18 +0100

    Added Day 8 also template for day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,19 @@
+﻿var inputData = File.ReadAllLines("input.txt");
+
+Console.WriteLine("*** AdventOfCode 2023 ***");
+Console.WriteLine("-------------------------");
+Console.WriteLine("* December 9th: *");
+Console.WriteLine("   Part #1");
+Console.WriteLine($"      Calculate some value : {RunPart1(inputData)}");
+Console.WriteLine("   Part #2");
+Console.WriteLine($"      Calculate some other value : {RunPart2(inputData)}");
+
+long RunPart1(string[] inputData)
+{
+    return 0;
+}
+
+long RunPart2(string[] inputData)
+{
+    return 0;
+}
