aaba352a8694f7100c000d7f9ed38812c67418bf
Andreas Kristiansen <andreas.kristiansen@pector.se>
2023-12-09 09:41:46 +0100

    Added day 9 also template for day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,20 @@
+﻿var inputData = File.ReadAllLines("input.txt");
+
+Console.WriteLine("*** AdventOfCode 2023 ***");
+Console.WriteLine("-------------------------");
+Console.WriteLine("* December 10th: *");
+Console.WriteLine("   Part #1");
+Console.WriteLine($"      Sum of somevalue : {RunPart1(inputData)}");
+Console.WriteLine("   Part #2");
+Console.WriteLine($"      Sum of some other value : {RunPart2(inputData)}");
+Console.WriteLine("-------------------------");
+
+long RunPart1(string[] inputData)
+{
+    return 0;
+}
+
+long RunPart2(string[] inputData)
+{
+    return 0;
+}
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,19 +1,57 @@
-﻿var inputData = File.ReadAllLines("input.txt");
+﻿using AoCToolbox;
+
+var inputData = File.ReadAllLines("input.txt");
 
 Console.WriteLine("*** AdventOfCode 2023 ***");
 Console.WriteLine("-------------------------");
 Console.WriteLine("* December 9th: *");
 Console.WriteLine("   Part #1");
-Console.WriteLine($"      Calculate some value : {RunPart1(inputData)}");
+Console.WriteLine($"      Sum of these extrapolated values(Forward) : {RunPart1(inputData)}");
 Console.WriteLine("   Part #2");
-Console.WriteLine($"      Calculate some other value : {RunPart2(inputData)}");
+Console.WriteLine($"      Sum of these extrapolated values(Backward) : {RunPart2(inputData)}");
+Console.WriteLine("-------------------------");
 
 long RunPart1(string[] inputData)
 {
-    return 0;
+    var histories = inputData.Select(FormHistory);
+
+    return histories.Sum(ExtrapolateForwards);
 }
 
 long RunPart2(string[] inputData)
 {
-    return 0;
+    var histories = inputData.Select(FormHistory);
+    return histories.Sum(ExtrapolateBackwards);
+}
+
+List<int[]> FormHistory(string report)
+{
+    var initial = report.ParseInts();
+    var sequences = new List<int[]> { initial.ToArray() };
+
+    while (sequences[^1].Any(v => v != 0))
+    {
+        sequences.Add(item: sequences[^1]
+            .Skip(1)
+            .Select((val, i) => val - sequences[^1][i])
+            .ToArray());
+    }
+
+    return sequences;
+}
+
+int ExtrapolateForwards(IList<int[]> sequences)
+{
+    return sequences
+        .Reverse()
+        .Skip(1)
+        .Aggregate(seed: 0, func: (n, seq) => n + seq[^1]);
+}
+
+int ExtrapolateBackwards(IList<int[]> sequences)
+{
+    return sequences
+        .Reverse()
+        .Skip(1)
+        .Aggregate(seed: 0, func: (n, seq) => seq[0] - n);
 }
